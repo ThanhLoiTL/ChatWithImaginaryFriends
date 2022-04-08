81fbc2a0bd1286e43f81c8ff371e3094c32906e2
huykaito454 <tronghuy.832001@gmail.com>
2022-04-08 17:31:35 +0700

    commit

--- app/src/main/java/com/android/chatwithimaginaryfriends/MainActivity.java
+++ app/src/main/java/com/android/chatwithimaginaryfriends/MainActivity.java
@@ -2,13 +2,27 @@ package com.android.chatwithimaginaryfriends;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void ShowHeartActivity(MenuItem item) {
+        Intent intent = new Intent(this,HeartActivity.class);
+        startActivity(intent);
     }
 }
