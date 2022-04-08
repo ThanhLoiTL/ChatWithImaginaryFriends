042b50f9785420289a9154cfabb08169c9480984
Thanh Loi <thanhloi030301@gmail.com>
2022-04-08 17:11:55 +0700

    first commit 1

--- app/src/main/java/com/android/chatwithimaginaryfriends/MainActivity.java
+++ app/src/main/java/com/android/chatwithimaginaryfriends/MainActivity.java
@@ -1,14 +1,51 @@
 package com.android.chatwithimaginaryfriends;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView navigationMenu;
+    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navigationMenu = findViewById(R.id.bottom_navigation);
+        viewPager = findViewById(R.id.view_pager);
+
+        navigationMenu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_heart:
+                        viewPager.setCurrentItem(0);
+                        break;
+                    case R.id.action_character:
+                        viewPager.setCurrentItem(1);
+                        break;
+                    case R.id.action_chat:
+                        viewPager.setCurrentItem(2);
+                        break;
+                    case R.id.action_setting:
+                        viewPager.setCurrentItem(3);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void setUpViewPager() {
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, this);
+        viewPager.setAdapter(viewPagerAdapter);
     }
 }
---
+++ app/src/main/java/com/android/chatwithimaginaryfriends/ViewPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.android.chatwithimaginaryfriends;
+
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter  {
+    private final Context mContext;
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior, Context mContext) {
+        super(fm, behavior);
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new HeartFragment();
+            case 1:
+                return new CharacterFragment();
+            case 3:
+                return new SettingFragment();
+            case 2:
+            default:
+                return new ChatFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
